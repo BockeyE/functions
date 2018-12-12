d0238bda700fa58700442fd77f26d140cb5e24c2
b <gymnopediece@163.com>
2018-12-12 15:54:19 +0800

    go on

---
+++ src/Sorts/RandomArrUtil.java
@@ -0,0 +1,14 @@
+package Sorts;
+
+import java.util.Random;
+
+public class RandomArrUtil {
+    public static int[] getArr(int len, int max) {
+        int[] ar = new int[len];
+        Random random = new Random();
+        for (int i : ar) {
+            i = random.nextInt(max);
+        }
+        return ar;
+    }
+}
