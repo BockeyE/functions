ae56ffafb6487e1b97c3dd41a9ebc80b8d074a9c
b <gymnopediece@163.com>
2018-12-17 09:58:59 +0800

    lamda

---
+++ src/lamda/ThisDemo.java
@@ -0,0 +1,29 @@
+package lamda;
+
+import org.omg.CORBA.StringHolder;
+
+/**
+ * @author bockey
+ */
+public class ThisDemo {
+    private String name = "ThisDemo";
+
+    public void test() {
+        new Thread(new Runnable() {
+            private String name = "runnable";
+
+            @Override
+            public void run() {
+                System.out.println("current this is : " + this.name);
+            }
+        }).start();
+        new Thread(() -> {
+            System.out.println(" current this is : " + this.name
+            );
+        }).start();
+    }
+
+    public static void main(String[] args) {
+         new ThisDemo().test();
+    }
+}
