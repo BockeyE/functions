847285f375432535e5607c9b8efedcff669bb858
b <gymnopediece@163.com>
2018-12-19 17:39:26 +0800

    go on

--- src/hashCollision/AllArrangement.java
+++ src/hashCollision/AllArrangement.java
@@ -1,18 +1,26 @@
 package hashCollision;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author bockey
+ * un repeatable arrangement
  */
 public class AllArrangement {
     public static void main(String[] args) {
 
-        char[] ss = {'a', 'b', 'c','d'};
-        tset(ss, 0);
+        char[] ss = {'a', 'b', 'c', 'd'};
+        nosort(ss);
 
     }
 
-    public static void tset(char[] ss, int i) {
+    public static void unrepeat(char[] ss) {
+        unrepeatSub(ss, 0);
+    }
+
+    public static void unrepeatSub(char[] ss, int i) {
         if (ss == null || i < 0 || i > ss.length) {
             return;
         }
@@ -23,11 +31,36 @@ public class AllArrangement {
                 char temp = ss[j];
                 ss[j] = ss[i];
                 ss[i] = temp;
-                tset(ss, i + 1);
+                unrepeatSub(ss, i + 1);
                 temp = ss[j];
                 ss[j] = ss[i];
                 ss[i] = temp;
             }
         }
+    }
+
+    public static void nosort(char[] chars) {
+        if (chars == null || chars.length == 0) {
+            return;
+        }
+        List<Character> list = new ArrayList<>();
+        for (int i = 0; i <= chars.length; i++) {
+            nosortSub(chars, 0, i, list);
+        }
+
+    }
+
+    public static void nosortSub(char[] chars, int begin, int num, List<Character> list) {
+        if (num == 0) {
+            System.out.println(list);
+            return;
+        }
+        if (begin == chars.length) {
+            return;
+        }
+        list.add(chars[begin]);
+        nosortSub(chars, begin + 1, num - 1, list);
+        list.remove((Character) chars[begin]);
+        nosortSub(chars, begin + 1, num, list);
     }
 }
