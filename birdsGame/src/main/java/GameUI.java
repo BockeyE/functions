cbb3a3702c9cb7bcbe34bc112de988c2e73add30
b <gymnopediece@163.com>
2019-01-02 18:46:53 +0800

    init

---
+++ birdsGame/src/main/java/GameUI.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+/**
+ * @author bockey
+ */
+public class GameUI extends JFrame implements MouseListener, MouseMotionListener {
+    /*
+     * 游戏界面组成
+     * gamePanel : 游戏画面的容器，也是画板，主要绘制游戏界面
+     * bird 	 : 游戏中的主角，即小鸟
+     * pipe		 : 游戏中的障碍物，即管道
+     * score 	 : 游戏得分
+     */
+
+
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
