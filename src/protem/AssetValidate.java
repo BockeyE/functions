becadc755c6cafcd6f422bd1e4f5a7ad97b0c300
b <gymnopediece@163.com>
2019-02-14 19:36:44 +0800

    go on

---
+++ src/protem/AssetValidate.java
@@ -0,0 +1,14 @@
+package protem;
+
+import java.util.Date;
+
+/**
+ * @author bockey
+ */
+public class AssetValidate {
+    String assetId;
+    int invoiceCheck;
+    Date invoiceCheckDate;
+    String ZDWsearch;
+    String ZDWregist;
+}
