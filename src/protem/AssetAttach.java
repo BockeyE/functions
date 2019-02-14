8bf6268c23e263d519902db4b55cf74078f081a1
b <gymnopediece@163.com>
2019-02-14 19:36:35 +0800

    go on

---
+++ src/protem/AssetAttach.java
@@ -0,0 +1,15 @@
+package protem;
+
+import java.util.Date;
+
+/**
+ * @author bockey
+ */
+public class AssetAttach {
+    String assetId;
+    String attachType;
+    Date uploadTime;
+    String attachNumber;
+    String fileMD5;
+
+}
