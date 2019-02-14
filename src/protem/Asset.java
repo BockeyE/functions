1e5d648bd862e7176c09db767c1ec4ea79031024
b <gymnopediece@163.com>
2019-02-14 19:36:31 +0800

    go on

---
+++ src/protem/Asset.java
@@ -0,0 +1,31 @@
+package protem;
+
+/**
+ * @author bockey
+ */
+public class Asset {
+    String projectId;
+    String assetId;
+    String buyer;
+    String seller;
+    String amount;
+    String billNumber;
+    String billExpireDate;
+    String invoiceNumber;
+    String invoiceAmount;
+    String contractNumber;
+    String contractFileName;
+    String assetFileName;
+    int status;
+    String attr1;
+    String attr2;
+    String attr3;
+    String attr4;
+    String attr5;
+    String attr6;
+    String attr7;
+    String attr8;
+    String attr9;
+    String attr10;
+
+}
