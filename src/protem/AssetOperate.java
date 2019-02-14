55af1a32eaee40eed78593f5a2b866416748c1b5
b <gymnopediece@163.com>
2019-02-14 19:36:39 +0800

    go on

---
+++ src/protem/AssetOperate.java
@@ -0,0 +1,15 @@
+package protem;
+
+import java.util.Date;
+
+/**
+ * @author bockey
+ */
+public class AssetOperate {
+    String assetId;
+    String operator;
+    String operateType;
+    String operateResult;
+    String description;
+    Date operateTime;
+}
