70e31a630c93b17871af364086398377efa6ff59
b <gymnopediece@163.com>
2019-02-15 11:16:50 +0800

    go on

---
+++ src/protem/AssetVO.java
@@ -0,0 +1,18 @@
+package protem;
+
+/**
+ * @author bockey
+ */
+public class AssetVO {
+    String buyer;
+    String seller;
+    String amount;
+    String billNumber;
+    String billExpireDate;
+    String invoiceNumber;
+    String invoiceAmount;
+    String contractNumber;
+    String contractFileName;
+    String assetFileName;
+    int status;
+}
