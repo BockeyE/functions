5cf323d4bccfe932fb381020fdd6f8c194bfefad
b <gymnopediece@163.com>
2019-02-21 11:19:15 +0800

    go on

---
+++ src/protem/ProjectAssetCount.java
@@ -0,0 +1,27 @@
+package protem;
+
+/**
+ * @author bockey
+ */
+public class ProjectAssetCount {
+    int totalAssets;
+    int totalAmount;
+    int unfilteredAssets;
+    int unfilteredAmount;
+    int filterFailedAssets;
+    int filterFailedAmount;
+    int filterPassedAssets;
+    int filterPassedAmount;
+    int confirmPassedAssets;
+    int confirmPassedAmount;
+    int confirmFailedAssets;
+    int confirmFailedAmount;
+    int aplliedAssets;
+    int aplliedAmount;
+    int approvalAssets;
+    int approvalAmount;
+    int applieFailedAssets;
+    int applieFailedAmount;
+    int publishedAssets;
+    int publishedAmount;
+}
