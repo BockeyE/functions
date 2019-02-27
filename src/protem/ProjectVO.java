22d3cacaf6e38f0579ad9e52a425b3b58332a082
b <gymnopediece@163.com>
2019-02-27 09:41:51 +0800

    go on

---
+++ src/protem/ProjectVO.java
@@ -0,0 +1,17 @@
+package protem;
+
+/**
+ * @author bockey
+ */
+public class ProjectVO {
+    String projectId;
+    String projectNumber;
+    String projectName;
+    String coreCompany;
+    String planName;
+    String publishDate;
+    String expireDate;
+    String scale;
+    String status;
+
+}
