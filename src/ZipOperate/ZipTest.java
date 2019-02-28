dd7203cd48f6a52d467979c475cc15e6527a8040
b <gymnopediece@163.com>
2019-02-28 10:38:39 +0800

    go on

--- src/ZipOperate/ZipTest.java
+++ src/ZipOperate/ZipTest.java
@@ -10,14 +10,9 @@ import java.util.zip.*;
 public class ZipTest {
 
     public static void main(String[] args) throws IOException {
-
         String zf = ("C:\\Users\\user1\\Desktop\\bh.jpg");
-
         String aim = ("C:\\Users\\user1\\Desktop\\front\\auto222.zip");
-
         compressFile(zf, aim);
-
-
         String dezip = "C:\\Users\\user1\\Desktop\\auto\\a.jpg";
         decompressFile(aim, dezip);
     }
@@ -25,38 +20,29 @@ public class ZipTest {
     public static void compressFile(String srcf, String destf) throws IOException {
         File src = new File(srcf);
         File dest = new File(destf);
-
         FileOutputStream fops = new FileOutputStream(dest);
         CheckedOutputStream ccos = new CheckedOutputStream(fops, new CRC32());
 //        这是两个过滤流，用于维护数据校验和。校验和是用于维护数据完整性的一项技术。
 //        先来简单聊聊校验和，校验和(checksum)，是冗余校验的一种形式。
 //        它是通过错误检测方法，对经过空间（如通信）或者时间（如计算机存储）传送的数据的完整性进行检查的一种简单方法。
 //        循环冗余校验CRC
-
         ZipOutputStream zos = new ZipOutputStream(ccos);
-
-
         //zipentry 是表示在压缩文件内的条目名称，带路径的话压缩包内也会是分层的
         //文件输入由ips读到byte[]中决定，不需要在这里指定
         ZipEntry ze = new ZipEntry("bh.jpg");
-
         zos.putNextEntry(ze);
-
         FileInputStream fip = new FileInputStream(src);
+        streamDo(zos, fip);
+        closeAll(zos,fip,fops,ccos);
 
+    }
+
+    private static void streamDo(OutputStream zos, InputStream fip) throws IOException {
         byte[] tem = new byte[1024];
         int read = 0;
-
         while ((read = fip.read(tem)) != -1) {
             zos.write(tem, 0, read);
-
         }
-
-        zos.close();
-        fip.close();
-        fops.close();
-        ccos.close();
-
     }
 
     public static void decompressFile(String srcf, String destf) throws IOException {
@@ -64,26 +50,27 @@ public class ZipTest {
         ZipFile zf = new ZipFile(srf);
         Enumeration<? extends ZipEntry> entries = zf.entries();
         ZipEntry entry = null;
-
 //        while (entries.hasMoreElements()) {
         entry = entries.nextElement();
         String path = destf + File.separator + entry.getName();
         File aim = new File(path);
         InputStream inputStream = zf.getInputStream(entry);
         FileOutputStream fop = new FileOutputStream(destf);
-
-        byte[] tem = new byte[1024];
-        int read = 0;
-        while ((read = inputStream.read(tem)) != -1) {
-            fop.write(tem, 0, read);
-        }
+        streamDo(fop, inputStream);
         inputStream.close();
         fop.close();
-
-
 //        }
-
     }
 
+    private static void closeAll(Closeable... c) {
+        for (Closeable closeable : c) {
+            try {
+                closeable.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
 }
