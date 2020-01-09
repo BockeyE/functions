d5c077cb5458fdd520d1a9428e57f95ede2b0ed1
b <gymnopediece@163.com>
2020-01-09 09:59:10 +0800

    annot

---
+++ src/AAAAAAPracs/springboot_annotation/RequiredPermissions.java
@@ -0,0 +1,17 @@
+package AAAAAAPracs.springboot_annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 自定义的注解
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequiredPermissions {
+
+    int[] value() default {};
+
+}
