3a497345553574f52c4f28b6a48c771b7ee2a608
b <gymnopediece@163.com>
2020-02-10 15:34:50 +0800

    jpa

--- src/AAAAAAPracs/JPAExample/eg1.java
+++ src/AAAAAAPracs/JPAExample/eg1.java
@@ -1,5 +1,6 @@
 package AAAAAAPracs.JPAExample;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -40,6 +41,41 @@ public class eg1 {
 //            return criteriaQuery.getRestriction();
 //        }
 //    }, pageable);
+
+//    public List<OnSiteSign> querySignsForArchive(String projName, String startDate, String endDate, List<String> procesIds) {
+//        该方法，意在查询出所有时间区间内的，状态是完成的，属于本项目，且processid不在已归档的list中
+//        return signDao.findAll((Specification<OnSiteSign>) (root, criteriaQuery, cb) -> {
+//            Predicate p3 = cb.equal(root.get("projectName").as(String.class), projName);
+//            Predicate p1 = cb.greaterThanOrEqualTo(root.get("dateStr").as(String.class), startDate);
+//            CriteriaBuilder.In<String> in = cb.in(root.get("processId"));
+//            for (String id : procesIds) {
+//                in.value(id);
+//            }
+//            Predicate p2 = cb.lessThanOrEqualTo(root.get("dateStr").as(String.class), endDate);
+//            criteriaQuery.where(cb.and(cb.not(in), p3, p1, p2));
+//            criteriaQuery.orderBy(cb.desc(root.get("id").as(Long.class)));
+//            return criteriaQuery.getRestriction();
+//        });
+//    }
+
+//    public List<OnSiteSign> querySignsForArchive(String projName, Date startDate, Date endDate, List<String> procesIds) {
+//
+//        System.out.println("cb test");
+//        该方法，意在查询出所有时间区间内的，状态是完成的，属于本项目，且processid不在已归档的list中
+//        return signDao.findAll((Specification<OnSiteSign>) (root, criteriaQuery, cb) -> {
+//            Predicate p2 = cb.lessThanOrEqualTo(root.get("addDate").as(Date.class), endDate);
+//            Predicate p3 = cb.equal(root.get("projectName").as(String.class), projName);
+//            Predicate p2 = cb.greaterThanOrEqualTo(root.get("dateStr").as(String.class), startDate.toString());
+//            Predicate p1 = root.get("processId").in(procesIds);
+//            if (procesIds.size() == 0) {
+//                criteriaQuery.where(cb.and(p3, p2));
+//            } else {
+//                criteriaQuery.where(cb.and(p3, p2, p1));
+//            }
+//            criteriaQuery.orderBy(cb.asc(root.get("id").as(Long.class)));
+//            return criteriaQuery.getRestriction();
+//        });
+//    }
 
 
 }
