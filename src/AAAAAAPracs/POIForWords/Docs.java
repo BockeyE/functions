2489977b915118ecc75ffc3cf4cf39741a928aa3
b <gymnopediece@163.com>
2020-02-17 17:14:48 +0800

    doc

--- src/AAAAAAPracs/POIForWords/Docs.java
+++ src/AAAAAAPracs/POIForWords/Docs.java
@@ -88,5 +88,33 @@ public class Docs {
 //        }
 //    }
 
+
+
+
+
+
+
+
+
+    //XWPFParagraph 段落属性
+//paragraphX.addRun(runX0);//似乎并没有什么卵用
+//paragraphX.removeRun(1);//按数组下标删除run(文本)
+//paragraphX.setAlignment(ParagraphAlignment.LEFT);//对齐方式
+//paragraphX.setBorderBetween(Borders.LIGHTNING_1);//边界 （但是我设置了好几个值都没有效果）
+//paragraphX.setFirstLineIndent(100);//首行缩进：-----效果不详
+//paragraphX.setFontAlignment(3);//字体对齐方式：1左对齐 2居中3右对齐
+//paragraphX.setIndentationFirstLine(2);//首行缩进：-----效果不详
+//paragraphX.setIndentationHanging(1);//指定缩进，从父段落的第一行删除，将第一行上的缩进移回到文本流方向的开头。-----效果不详
+//paragraphX.setIndentationLeft(2);//-----效果不详
+//paragraphX.setIndentationRight(2);//-----效果不详
+//paragraphX.setIndentFromLeft(2);//-----效果不详
+//paragraphX.setIndentFromRight(2);//-----效果不详
+//paragraphX.setNumID(new BigInteger("3"));//设置段落编号-----有效果看不懂（仅仅是整段缩进4个字）
+//paragraphX.setPageBreak(true);//段前分页
+//paragraphX.setSpacingAfter(1);//指定文档中此段最后一行以绝对单位添加的间距。-----效果不详
+//paragraphX.setSpacingBeforeLines(2);//指定在该行的第一行中添加行单位之前的间距-----效果不详
+//paragraphX.setStyle("标题 3");//段落样式：需要结合addCustomHeadingStyle(docxDocument, "标题 3", 3)配合使用
+//paragraphX.setVerticalAlignment(TextAlignment.BOTTOM);//文本对齐方式(我猜在table里面会有比较明显得到效果)
+//paragraphX.setWordWrapped(true);//这个元素指定一个消费者是否应该突破拉丁语文本超过一行的文本范围，打破单词跨两行（打破字符水平）或移动到以下行字（打破字级）-----(我没看懂:填个false还报异常了)
 }
 
