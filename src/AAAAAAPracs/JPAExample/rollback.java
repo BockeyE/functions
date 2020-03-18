9223e7163153722d343fec1ac03f3ff3b507e56f
b <gymnopediece@163.com>
2020-03-18 16:50:37 +0800

    change

---
+++ src/AAAAAAPracs/JPAExample/rollback.java
@@ -0,0 +1,25 @@
+package AAAAAAPracs.JPAExample;
+
+/**
+ * @author bockey
+ */
+public class rollback {
+//    @Transactional(rollbackFor = Exception.class)
+////返回Object类型的带参方法，抛出一个异常
+//    public Object test(Integer i) throws Exception{
+//        //创建实体类的实例，通过实例向数据库中添加数据
+//        InterfaceTest interfaceTest=new InterfaceTest();
+//        interfaceTest.setInterfaceName("qwer");
+//        interfaceTest.setInterfaceMethod("GET");
+//        interfaceTest.setInterfaceType("HTTP");
+//        interfaceTest.setInterfaceAliasName("QWER");
+//        //接口类实例调用接口的新增方法，添加数据到数据库
+//        int i=myInterfaceTestMapper.addInterfaceTest(interfaceTest);
+//        //如果方法传参i不等于1就抛出异常，若等于1就正常返回，添加成功
+//        if(i!=1){
+//            throw new Exception();
+//        }else {
+//            return "添加成功";
+//        }
+//    }
+}
