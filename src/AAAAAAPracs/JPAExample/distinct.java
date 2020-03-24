3a8f4de4ea9355515fae1eb6037eb77ce9712f8d
b <gymnopediece@163.com>
2020-03-24 17:09:26 +0800

    change

---
+++ src/AAAAAAPracs/JPAExample/distinct.java
@@ -0,0 +1,20 @@
+package AAAAAAPracs.JPAExample;
+
+/**
+ * @author bockey
+ */
+public class distinct {
+//    {
+//        signInfoDao.findAll(new Specification<OnSiteSignInfo>() {
+//            @Override
+//            public Predicate toPredicate(Root<OnSiteSignInfo> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
+//                Predicate p4 = cb.greaterThanOrEqualTo(root.get("dateStr").as(String.class), "%" + key + "%");
+//                Predicate p3 = cb.lessThanOrEqualTo(root.get("dateStr").as(String.class), "%" + key + "%");
+//                criteriaQuery.where(cb.or(p3, p4));
+//                criteriaQuery.orderBy(cb.desc(root.get("id").as(Long.class)));
+//                criteriaQuery.select(root.get("processId")).distinct(true);
+//                return criteriaQuery.getRestriction();
+//            }
+//        });
+//    }
+}
