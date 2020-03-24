393012abfa04baedb7f2e3bbc39eadbae477b1d4
b <gymnopediece@163.com>
2020-03-24 17:20:13 +0800

    change

---
+++ src/AAAAAAPracs/JPAExample/local.java
@@ -0,0 +1,17 @@
+package AAAAAAPracs.JPAExample;
+
+/**
+ * @author bockey
+ */
+public class local {
+//    @Modifying
+//    @Transactional
+//    @Query(value = "create table acms_asset_:tableName (asset_id integer not null auto_increment, amount DECIMAL(19,2), " +
+//            "asset_filename varchar(255), asset_status integer, attachment_upload_status integer,attr0 varchar(255), attr1 varchar(255), " +
+//            "attr10 varchar(255), attr2 varchar(255), attr3 varchar(255), attr4 varchar(255), " +
+//            "attr5 varchar(255), attr6 varchar(255), attr7 varchar(255), attr8 varchar(255), " +
+//            "attr9 varchar(255), bill_number varchar(255), buyer varchar(255), contract_number varchar(255), " +
+//            "due_date date, invoice_amount varchar(255), invoice_number varchar(255), " +
+//            "perform_filename varchar(255), proj_id integer, seller varchar(255), primary key (asset_id))", nativeQuery = true)
+//    void createTable(@Param("tableName") Integer tableName);
+}
