6d7629ea7772eb3d2b25b39fc46593c426c220ec
b <gymnopediece@163.com>
2020-04-10 10:55:56 +0800

    change

---
+++ src/AAAAAAPracs/JPAExample/mybatis/te1.java
@@ -0,0 +1,11 @@
+package AAAAAAPracs.JPAExample.mybatis;
+
+import java.math.BigInteger;
+
+/**
+ * @author bockey
+ */
+public class te1 {
+//    @Select("select ifNull(sum(amount),0) from customer_points where owner_phone=#{phone}")
+//    BigInteger getBalanceByUphone(String phone);
+}
