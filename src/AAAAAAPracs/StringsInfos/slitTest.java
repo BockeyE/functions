49413d548d5d6a1f7f0eabf157e97ab2db20c6e1
b <gymnopediece@163.com>
2020-05-29 16:21:42 +0800

    change

---
+++ src/AAAAAAPracs/StringsInfos/slitTest.java
@@ -0,0 +1,10 @@
+package AAAAAAPracs.StringsInfos;
+
+/**
+ * @author bockey
+ */
+public class slitTest {
+    public static void main(String[] args) {
+        String[] split = "attachHash".split("\\|");
+    }
+}
