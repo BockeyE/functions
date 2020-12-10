5cae6c60c3b9f26968d37cd6327ee1e574528627
b <598449667@qq.com>
2020-12-10 17:34:05 +0800

    pool

--- src/localThreadPoolDemo/TestForkJoin.java
+++ src/localThreadPoolDemo/TestForkJoin.java
@@ -30,3 +30,31 @@ class LocalFJTask extends RecursiveTask<Integer> {
         }
     }
 }
+
+class LocalFJTask2 extends RecursiveTask<Integer> {
+    int begin;
+    int end;
+
+    public LocalFJTask2(int a, int b) {
+        begin = a;
+        end = b;
+    }
+
+    @Override
+    public Integer compute() {
+        if (begin == end) {
+            return begin;
+        } else if (end - begin == 1) {
+            return end + begin;
+        } else {
+            int mid = (begin + end) / 2;
+            LocalFJTask2 task1 = new LocalFJTask2(begin, mid);
+            task1.fork();
+            LocalFJTask2 task2 = new LocalFJTask2(mid + 1, end);
+            //让一个线程去执行
+            task2.fork();
+            //获取结果
+            return task1.join() + task2.join();
+        }
+    }
+}
